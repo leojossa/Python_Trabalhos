7b8b90d7676d65beda7180431f934cd05ea93238
leojossa <62522332+leojossa@users.noreply.github.com>
2020-09-24 18:44:20 -0300

    Update app.py

--- Flask - Front End/app.py
+++ Flask - Front End/app.py
@@ -30,39 +30,22 @@ SESSION_REFRESH_EACH_REQUEST = True #informa que a sessao tera refresh
 def home():
     return render_template('home1.html')
 
-'''
-@app.route('/gerasenha', methods=['GET','POST'])
-def gerasenha():
-    if request.method == 'POST':
-        random_source = string.ascii_letters + string.digits  # + string.punctuation
-        password = random.choice(string.ascii_lowercase)
-        password += random.choice(string.ascii_uppercase)
-        password += random.choice(string.digits)
-        # password += random.choice(string.punctuation)
-        for i in range(6):
-            password += random.choice(random_source)
-        password_list = list(password)
-        random.SystemRandom().shuffle(password_list)
-        password = ''.join(password_list)
-        return {'senha': password}
-    return render_template('gerasenha.html')
-'''
-@app.route('/register', methods=['GET','POST'])
+@app.route('/xxx', methods=['GET','POST'])
 def alteraUsuario():
-    metadata.reflect(schema='login')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         if request.method == 'POST':
-            username = request.form.get('username')
-            passwordold = request.form.get('passwordold')
-            password = request.form.get('password')
-            confirm = request.form.get('confirm')
-            passworddata = conn.execute('select password from login.users2 where username=%s', username).fetchone()
+            username = request.form.get('xxx')
+            passwordold = request.form.get('xxx')
+            password = request.form.get('xxx')
+            confirm = request.form.get('xxx')
+            passworddata = conn.execute('select xxx from xxx.xxx where xxx=%s', username).fetchone()
             for password_data in passworddata:
                 if sha256_crypt.verify(passwordold, password_data):
                     session['log'] = True
                     secure_password = sha256_crypt.encrypt(str(password))
                     if password == confirm:
-                        conn.execute('update login.users2 set username=%s, password=%s where username=%s', username,
+                        conn.execute('update xxx.xxx set xxx=%s, xxx=%s where xxx=%s', username,
                                      secure_password, username)
                     #return redirect(url_for('login'))
                     flash('Registro efetuado', 'success')
@@ -70,44 +53,16 @@ def alteraUsuario():
             else:
                 return redirect(url_for('login'))
         return render_template('register1.html')
-#def register():
-#    metadata.reflect(schema='login')
-#    with engine.begin() as conn:
-#        if request.method == 'POST':
-#            name = request.form.get('name')
-#            username = request.form.get('username')
-#            password = request.form.get('password')
-#            confirm = request.form.get('confirm')
-#            secure_password = sha256_crypt.encrypt(str(password))
 
-#            usernamedata = conn.execute('select username from login.users2 where username=%s', username).fetchone()
-
-#            if usernamedata == None:
-#                if password == confirm:
-#                    conn.execute('insert into login.users2 (name, username, password) values (%s, %s, %s)', name, username, secure_password)
-#                    flash('Registro efetuado', 'success')
-#                    return redirect(url_for('login'))
-                    #return redirect(url_for('restplus_doc.static', filename='api'))
-#                else:
-#                    flash('usuario já existe, contate o administrador', 'danger')
-#                    return redirect(url_for('login'))
-#        return render_template('register1.html')
-
-user = []
-senha = []
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/xxx', methods=['GET', 'POST'])
 def login():
-    metadata.reflect(schema='login')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         if request.method == 'POST':
-            username = request.form.get('name')
-            password = request.form.get('password')
-            senha.clear()
-            senha.append(password)
-            user.clear()
-            user.append(username)
-            usernamedata = conn.execute('select username from login.users2 where username=%s', username).fetchone()
-            passworddata = conn.execute('select password from login.users2 where username=%s', username).fetchone()
+            username = request.form.get('xxx')
+            password = request.form.get('xxx')
+            usernamedata = conn.execute('select xxx from xxx.xxx where xxx=%s', username).fetchone()
+            passworddata = conn.execute('select xxx from xxx.xxx where xxx=%s', username).fetchone()
 
             if usernamedata is None:
                 flash('digite o usuario', 'danger')
@@ -117,30 +72,23 @@ def login():
                     if sha256_crypt.verify(password, password_data):
                         session['log'] = True
                         token = jwt.encode(
-                            {'user': request.form['name'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                            {'user': request.form['xxx'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
                             app.config['SECRET_KEY'])
-                        #return jsonify({'token': token.decode('UTF-8')})
-                        query = conn.execute('select id, name, username, email from login.users2')
-                        #result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
-                        #return str(result)
-                        #return render_template('tela.html', query = query)
-                        #return render_template('consulta.html')
+                        query = conn.execute('select xxx, xxx, xxx, xxx from xxx.xxx')
                         flash('Login Efetuado', 'success')
                         return redirect(url_for('login'))
 
                 else:
                     flash('senha incorreta', 'danger')
                     return redirect(url_for('login'))
-                    #return render_template('login.html')
-                    #return make_response('nao verificado', 401, {'WWW-Authenticate': 'Basic realm="Login Necessario"'})
         return render_template('login1.html')
 
 #código para trazer a área do usuario
 def area():
-    metadata.reflect(schema='login')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         username = user
-        query = conn.execute('select area from users2 where name=%s', username)
+        query = conn.execute('select xxx from xxx where xxx=%s', username)
         result = [i[0] for i in query.cursor]
         return result
     return area
@@ -151,48 +99,46 @@ def token_required(f):
     def decoreted(*args, **kwargs):
         token = senha
         if not token:
-            #return jsonify({'message': 'Efetue o Login'}), 403
             flash('Efetue o login')
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decoreted
 
 lista = []
-@app.route('/consulta', methods=['GET', 'POST'])
+@app.route('/xxx', methods=['GET', 'POST'])
 @token_required
 def consulta():
-    metadata.reflect(schema='login')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         if request.method == 'POST':
-            name = request.form.get('name')
+            name = request.form.get('xxx')
             lista.clear() # limpa a lista
             lista.append(name) # faz apende do parametro na lista
-            query = conn.execute('select id, name, username, email from login.users2 where name=%s', name)
+            query = conn.execute('select xxx, xxx, xxx, xxx from xxx.xxx where xxx=%s', name)
             return render_template('tela.html', query=query)
 
         return render_template('consulta.html')
 
-@app.route('/consulta', methods=['GET', 'POST'])
+@app.route('/xxx', methods=['GET', 'POST'])
 @token_required
 def consulta1():
-    metadata.reflect(schema='login')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         if request.method == 'POST':
-            name = request.form.get('name')
-            query = conn.execute('select id, name, username, email from login.users2 where name=%s', name)
+            name = request.form.get('xxx')
+            query = conn.execute('select xxx, xxx, xxx, xxx from xxx.xxx where xxx=%s', name)
             result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
             return result
 
 #download em txt ou csv usando pandas e flask
-@app.route('/download')
+@app.route('/xxx')
 def download():
-    metadata.reflect(schema='login')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         name = lista
-        query = conn.execute('select id, name, username, email from login.users2 where name=%s', name)
+        query = conn.execute('select xxx, xxx, xxx, xxx from xxx.xxx where xxx=%s', name)
         result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
         df = pd.DataFrame(result)
-        #df = pd.read_sql_query(sql='select id, name, username, email from login.users2', con=conn)
         response = make_response(df.to_csv(index=False))
         response.headers.set("Content-Disposition", "attachment", filename="test.txt")
         response.headers["Content-Type"] = "txt/csv"
@@ -200,63 +146,57 @@ def download():
 
 dataini = []
 datafim = []
-@app.route('/consultadata', methods=['GET', 'POST'])
+@app.route('/xxx', methods=['GET', 'POST'])
 @token_required
 def consultadata():
-    metadata.reflect(schema='db')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         if request.method == 'POST':
-            datainicial = request.form.get('datainicial')
-            #datafinal = request.form.get('datafinal')
+            datainicial = request.form.get('xxx')
             #tratamento da data
             dataInicial = datetime.datetime.strptime(datainicial, '%d/%m/%Y').strftime('%Y-%m-%d')
-            #dataFinal = datetime.datetime.strptime(datafinal, '%d/%m/%Y').strftime('%Y-%m-%d' + ' ' + '99:99:99')
             dataini.clear()
             datafim.clear()
             dataini.append(dataInicial)
-            #datafim.append(dataFinal)
-            query = conn.execute("select nome, telefone, celular, datacadastro "
-                                 "from db.consumidor "
-                                 "where dataAlteracao >= %s", dataini)
+            datafim.append(dataFinal)
+            query = conn.execute("select xxx, xxx, xxx, xxx "
+                                 "from xxx.xxx "
+                                 "where xxx >= %s", dataini)
             return render_template('tela1.html', query=query)
         return render_template('consultadata.html')
 
-@app.route('/downloaddata')
+@app.route('/xxx')
 def downloaddata():
-    metadata.reflect(schema='login')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         datainicial = dataini
-        #datafinal = datafim
-        query = conn.execute('select nome, telefone, celular, datacadastro '
-                             'from db.consumidor '
-                             'where dataAlteracao >=%s', datainicial)
+        query = conn.execute('select xxx, xxx, xxx, xxx '
+                             'from xxx.xxx '
+                             'where xxx >=%s', datainicial)
         result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
         df = pd.DataFrame(result)
-        #df = pd.read_sql_query(sql='select id, name, username, email from login.users2', con=conn)
         response = make_response(df.to_csv(index=False))
         response.headers.set("Content-Disposition", "attachment", filename="test.txt")
         response.headers["Content-Type"] = "txt/csv"
         return response
 
-@app.route('/manutencao', methods=['GET', 'POST'])
+@app.route('/xxx', methods=['GET', 'POST'])
 @token_required
 def manutencao():
-    metadata.reflect(schema='db')
+    metadata.reflect(schema='xxx')
     with engine.begin() as conn:
         if request.method == 'POST':
-            nome = request.form.get('nome')
-            termouso = request.form.get('termouso')
+            nome = request.form.get('xxx')
+            termouso = request.form.get('xxx')
             areanova = area()
-            termousoapp = request.form.get('termousoapp')
-            query = conn.execute('INSERT INTO db.consumidor(nome, termoUso, termoUsoApp, username, area) '
+            termousoapp = request.form.get('xxx')
+            query = conn.execute('INSERT INTO db.consumidor(xxx, xxx, xxx, xxx, xxx) '
                                  'VALUES(%s, %s, %s, %s, %s)', nome, termouso, termousoapp, user, areanova)
-            #return redirect(url_for('manutencao'))
             return render_template('manutencao.html')
     return render_template('manutencao.html')
 
-@app.route('/logout')
+@app.route('/xxx')
 def logout():
-    #session.pop('log', None)
     senha.clear()
     return redirect('/')
 
